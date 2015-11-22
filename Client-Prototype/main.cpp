fc8a18dfa32e30795a40c52ffa8dcb44704ddb13
Viktor Holmer <viktorholmer@gmail.com>
2015-11-22 14:36:25 +0100

    made databasehandler even more general

--- Client-Prototype/main.cpp
+++ Client-Prototype/main.cpp
@@ -16,9 +16,12 @@ struct DataBaseHandler {
 	xml_document<> doc;
 	
 	const char* rootName;
-	const char* fileName;
+	
+       	const string extension = ".xml";
 
-	DataBaseHandler(const char* name) : rootName(name), fileName(name) {
+	const string fileName;
+
+	DataBaseHandler(const char* name) : rootName(name), fileName(string(name) + extension) {
 		xml_node<>* weapons = this->doc.allocate_node(node_element, this->rootName);
 		this->doc.append_node(weapons);
 		this->root = this->doc.first_node();
@@ -54,7 +57,7 @@ int main(int argc, char* argv[]) {
 	std::ios::sync_with_stdio(false);
 	cin.tie(0);
 	
-	DataBaseHandler wpn("weapon");
+	DataBaseHandler wpn("weapons");
 	wpn.setNode("sword");
 	wpn.setAttribute("sword", "damage", "15");
 	wpn.setNode("axe");
@@ -62,6 +65,15 @@ int main(int argc, char* argv[]) {
 	wpn.setValue("axe", "A sharp iron axe.");
 	wpn.setValue("sword", "An ancient bronze sword.");
 	wpn.createFile();
+
+	DataBaseHandler items("items");
+	items.setNode("apple");
+	items.setAttribute("apple", "heals", "20");
+	items.setNode("chainsword");
+	items.setAttribute("chainsword", "damage", "63");
+	items.setValue("apple", "A shiny red apple. Looks tasty.");
+	items.setValue("chainsword", "A mighty chain sword, forged on Holy Terra itself.");
+	items.createFile();
 
 	return 0;
 }
